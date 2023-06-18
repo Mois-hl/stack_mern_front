ffbc11af345b6c6db69c8d56e6dd5c04f8aade56
Mois-hl <moisesae182@gmail.com>
2023-06-17 18:38:20 -0600

    variables

--- src/api/tasks.api.js
+++ src/api/tasks.api.js
@@ -1,19 +1,19 @@
 import axios from "axios"
 
 export const getListTask = async () =>
-  await axios.get('http://localhost:4000/tasks')
+  await axios.get(import.meta.env.VITE_SERVER_URL+'/tasks')
 
 export const createTask = async (task) =>
-  await axios.post('http://localhost:4000/task', task)
+  await axios.post(import.meta.env.VITE_SERVER_URL+'/task', task)
 
 export const deleteTask = async (id) =>
-  await axios.delete(`http://localhost:4000/task/${id}`)
+  await axios.delete(import.meta.env.VITE_SERVER_URL+`/task/${id}`)
 
 export const getTaskDetail = async (id) =>
-  await axios.get(`http://localhost:4000/task/${id}`)
+  await axios.get(import.meta.env.VITE_SERVER_URL+`/task/${id}`)
 
 export const updateTask = async (id, task) =>
-  await axios.put(`http://localhost:4000/task/${id}`, task)
+  await axios.put(import.meta.env.VITE_SERVER_URL+`/task/${id}`, task)
 
 export const toggleTask = async (id, done) =>
-  await axios.put(`http://localhost:4000/task/${id}`, {done})
+  await axios.put(import.meta.env.VITE_SERVER_URL+`/task/${id}`, {done})
